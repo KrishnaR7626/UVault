5c068f527c13997c42480b56ac223ccc860a8fd7
krishnar7626 <krishnar7626@github.com>
2022-06-26 16:17:41 -0400

    Added libraries

--- PasswordManager.py
+++ PasswordManager.py
@@ -21,15 +21,3 @@ retrieve()
 
 
 
-
-# file_out = open("encryptedfile.bin", "wb")
-# [ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
-# file_out.close()
-
-# file_in = open("encryptedfile.bin", "rb")
-# nonce, tag, ciphertext = [ file_in.read(x) for x in (16, 16, -1) ]
-
-# key = hashlib.scrypt(password, salt=salt, n=2**14, r=8, p=1, dklen=32)
-# cipher = AES.new(key, AES.MODE_GCM, nonce)
-# data = cipher.decrypt_and_verify(ciphertext, tag)
-# print(data.decode('UTF-8')) 
