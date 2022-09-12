5027c78a965fb509a0f40c729f28067f21be73d3
krishnar7626 <krishnaraju0807@gmail.com>
2022-09-12 17:13:17 -0400

    Partially implemented testing for database functions saving progress

--- Python/DatabaseFunctions.py
+++ Python/DatabaseFunctions.py
@@ -1,11 +1,11 @@
 # Database Functions
 import sqlite3
-import Entry
+from Entry import Entry
 
 def createDatabase(Cursor):
     Cursor.execute("CREATE TABLE Passwords(purpose text, password text)")
     entry = Entry("checksum", "bccd30e889cb6af72091f5faf246c4f2b2e27fde2fcff73cf86440ce94810af5")
-    addEntry(entry)
+    addEntry(Cursor, entry)
 
 def addEntry(Cursor, entry):
     Cursor.execute("INSERT INTO Passwords VALUES(?, ?)", (entry.purpose, entry.password))
--- Tests/Test.py
+++ Tests/Test.py
@@ -6,8 +6,9 @@ os.chdir("..")
 path = os.getcwd()+'/Python'
 sys.path.insert(1, path)
 from Entry import Entry
-
-
+import DatabaseFunctions as DatabaseFunctions
+import PasswordGenerationFunctions as PasswordGenerationFunctions
+import DatabaseEncryptionFunctions as DatabaseEncryptionFunctions
 os.chdir("Tests")
 
 # sys.path.insert(1, '{}/Resources'.format(os.getcwd))
@@ -15,22 +16,23 @@ os.chdir("Tests")
 # sys.path.insert(1, '{}/Resources'.format(os.getcwd))
 # sys.path.insert(1, '{}/Resources'.format(os.getcwd))
 # sys.path.insert(1, '{}/Resources'.format(os.getcwd))
+Connection = sqlite3.connect('UVault.db') 
+Cursor = Connection.cursor()
 class UVaultTests(unittest.TestCase):
     def testDataBase(self):
-        pass
-        # createDatabase(Cursor)
-        # entry1 = Entry("User1", "Password1")
-        # entry2 = Entry("User2", "Password2")
-        # entry3 = Entry("User3", "Password3")
-        # entry4 = Entry("User4", "Password4")
-        # entry5 = Entry("User5", "Password5")
-        # entry6 = Entry("User6", "Password6")
-        # addEntry(Cursor, entry1)    
-        # addEntry(Cursor, entry2)
-        # addEntry(Cursor, entry3)
-        # addEntry(Cursor, entry4)
-        # addEntry(Cursor, entry5)
-        # addEntry(Cursor, entry6)
+        DatabaseFunctions.createDatabase(Cursor)
+        entry1 = Entry("User1", "Password1")
+        entry2 = Entry("User2", "Password2")
+        entry3 = Entry("User3", "Password3")
+        entry4 = Entry("User4", "Password4")
+        entry5 = Entry("User5", "Password5")
+        entry6 = Entry("User6", "Password6")
+        DatabaseFunctions.addEntry(Cursor, entry1)    
+        DatabaseFunctions.addEntry(Cursor, entry2)
+        DatabaseFunctions.addEntry(Cursor, entry3)
+        DatabaseFunctions.addEntry(Cursor, entry4)
+        DatabaseFunctions.addEntry(Cursor, entry5)
+        DatabaseFunctions.addEntry(Cursor, entry6)
 
 
     def testEntryObject(self):
@@ -52,23 +54,26 @@ class UVaultTests(unittest.TestCase):
         self.assertEqual(entry3.purpose , "User 9")    
         self.assertEqual(entry1.password , "Password 7")
         self.assertEqual(entry2.password , "Password2")
-        self.assertEqual(entry3.password , "Password 9")    
-        
+        self.assertEqual(entry3.password , "Password 9")
 
     def testPasswordGeneration(self):
         pass
 
     def testEncryption(self):
         pass
+    def cleanup(self):
+        os.remove("UVault.db")
+
     # Connection = sqlite3.connect('UVault.db') 
     # Cursor = Connection.cursor()
     # testDataBase()
     # testPasswordGeneration()
     # testEncryption()
     # os.remove("")
+    
 if __name__ == '__main__':
     unittest.main()
-
+    
 
 
 #     display("Select the number of the operation you would like to perform:", 0)
