4b122f0df4457761e9df5976c752de51ef1572b2
Krishna Raju <77706428+KrishnaR7626@users.noreply.github.com>
2022-09-29 23:00:06 -0400

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 #installs dependencies
+
+# uncomment correct line to install for your distro
+# Debian
+#sudo apt-get -y install python3
+#sudo apt -y install python3-pip 
+
+# RHEL
+#sudo dnf -y install python3
+#sudo dnf -y install python3            
+
+# Arch
+#sudo pacman -Sy python3
+#sudo pacman -Sy python-pip             
+
+# Open
+#sudo zypper -y install python3
+#sudo zypper -y install python3-pip
+
+
+pip install Colorama
+pip install pycryptodomex
+pip install hashlib
+pip install python-time
